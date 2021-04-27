80c1347f799190d6dbe16a4da071d7740421fa34
kotlyarDG <kotlyar.d.g@gmail.com>
2021-04-27 21:48:03 +0300

    third section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-// Проверка, загрузилась ли страница
-$(document).ready(function(){
 
+
+$(document).ready(function () {
+
+	function ibg() {
+
+		$.each($('.ibg'), function (index, val) {
+
+			if ($(this).find('img').length > 0) {
+				$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+			}
+
+		});
+	}
+
+	ibg();
 });
