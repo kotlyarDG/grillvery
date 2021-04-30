c08ae6f2fe6f6a1b88f2c16126bac90e3451b83a
kotlyarDG <kotlyar.d.g@gmail.com>
2021-04-30 12:06:18 +0300

    parallax elements

--- js/script.js
+++ js/script.js
@@ -71,4 +71,56 @@ $(document).ready(function () {
 	$('.select').select2({
 		minimumResultsForSearch: Infinity
 	});
+
+	console.log($('#l1').data('top'));
+
+	let elementsLeftTop = $('.parallax-img-left-top');
+	let elementsRightTop = $('.parallax-img-right-top');
+	let elementsLeftbottom = $('.parallax-img-left-bottom');
+	let elementsRightbottom = $('.parallax-img-right-bottom');
+
+
+
+	$(document).mousemove(function (e) {
+		// $('#l1').css({
+		// 	'top': 70 - (e.pageY / 100),
+		// 	'left': 100 - (e.pageX / 100)
+		// });
+
+		// console.log($('#l1').data('top'))
+
+
+		// $('.parallax-img').css({
+		// 	'top': +$(this).data('top') - (e.pageY / 100),
+		// 	'left': +$(this).data('left') - (e.pageX / 100)
+		// })
+
+		$.each(elementsLeftTop, function () {
+			$(this).css({
+				'top': $(this).data('top') - (e.pageY / 30),
+				'left': $(this).data('left') + (e.pageX / 90)
+			})
+		});
+
+		$.each(elementsRightTop, function () {
+			$(this).css({
+				'top': $(this).data('top') - (e.pageY / 70),
+				'right': $(this).data('right') - (e.pageX / 40)
+			})
+		});
+
+		$.each(elementsLeftbottom, function () {
+			$(this).css({
+				'bottom': $(this).data('bottom') + (e.pageY / 210),
+				'left': $(this).data('left') - (e.pageX / 20)
+			})
+		});
+
+		$.each(elementsRightbottom, function () {
+			$(this).css({
+				'bottom': $(this).data('bottom') - (e.pageY / 220),
+				'right': $(this).data('right') - (e.pageX / 150)
+			})
+		});
+	})
 });
